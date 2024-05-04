91179b526a53beda238cc8fef140c8a482b218e1
LIGMATV <danishnaufal049@gmail.com>
2024-05-04 14:37:31 +0700

    Update redirect

--- redirect/middleware.js
+++
@@ -1,7 +0,0 @@
-
-import RedirectApi from "./lib/index.js";
-import * as data from "./data.js";
-
-const redirectApi = new RedirectApi(data);
-
-export default redirectApi.handler.bind(redirectApi);
